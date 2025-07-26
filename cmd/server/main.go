2215f13f6b9cfa2fdc6e8c86f5440f90b51c7dcb
yanitski <yanitski@ph1.uni-koeln.de>
2025-07-26 15:18:08 -0600

    initial functional state to establish websocket connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+    "github.com/gorilla/websocket"
 )
 
 func main() {
@@ -24,13 +26,47 @@ type Connection struct {
     Port    string  `json:"port"`
 }
 
-func handleConnection(w http.ResponseWriter, r *http.Request) {
-    c := &Connection{}
-    decoder := json.NewDecoder(r.Body)
-    err := decoder.Decode(c)
-    if err != nil {
-        respondWithError(w, http.StatusInternalServerError, "unable to unmarshal connection", err)
-    }
-
-    respondWithJSON(w, http.StatusOK, c)
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool {
+        return true
+    },
 }
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        respondWithError(w, http.StatusInternalServerError, "unable to upgrade to websocket", err)
+    }
+    defer conn.Close()
+
+    remoteAddr := conn.RemoteAddr()
+    localAddr := conn.LocalAddr()
+
+    log.Println("New websocket connection made")
+    log.Printf("Local IP: %s\n", localAddr)
+    log.Printf("Remote IP: %s\n", remoteAddr)
+
+    // c := &Connection{}
+    // decoder := json.NewDecoder(r.Body)
+    // err = decoder.Decode(c)
+    // if err != nil {
+    //     respondWithError(w, http.StatusInternalServerError, "unable to unmarshal connection", err)
+    // }
+
+    // respondWithJSON(w, http.StatusOK, c)
+    
+    for {
+        _, message, err := conn.ReadMessage()
+        if err != nil {
+            log.Println("Error reading message from websocket.")
+            break
+        }
+        fmt.Printf("Received: %s\n", message)
+        err = conn.WriteMessage(websocket.TextMessage, message)
+        if err != nil {
+            log.Println("Error echoing received message")
+            break
+        }
+    }
+}
+
