80c7998a0739233a7f31f83979f88e51eec6ae39
yanitski <yanitski@ph1.uni-koeln.de>
2025-08-08 09:24:59 -0600

    use alternative method to close websocket

--- cmd/client/command_exit.go
+++ cmd/client/command_exit.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
 func commandExit(cfg *apiConfig, arg string) error {
     fmt.Println("Closing FTP direct... Goodbye!")
-    cfg.ws.WriteMessage(websocket.CloseMessage, []byte{})
+    cfg.ws.WriteMessage(websocket.TextMessage, []byte("Close"))
+    time.Sleep(2*time.Second)
     cfg.ctxCancel()
     return nil
 }
